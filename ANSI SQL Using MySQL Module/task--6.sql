29d0ee06d2f2d41ed1bcdfa8ea7f363de7453880
SaiShruti-04 <saishruti004@gmail.com>
2025-06-02 13:21:25 +0530

    Add files via upload

---
+++ ANSI SQL Using MySQL Module/task--6.sql
@@ -0,0 +1,7 @@
+SELECT e.title,
+       SUM(resource_type = 'pdf') AS pdfs,
+       SUM(resource_type = 'image') AS images,
+       SUM(resource_type = 'link') AS links
+FROM Events e
+LEFT JOIN Resources r ON e.event_id = r.event_id
+GROUP BY e.event_id;
